d63d6ec74bf0202ec4af5826ada9366af37230e6
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-05-27 09:51:16 -0300

    Add files via upload

--- exercício/Classe_e_objeto.js
+++ exercício/Classe_e_objeto.js
@@ -7,10 +7,16 @@ Modifique a classe Heroi para que o construtor também receba e armazene esse n�
  no painel de resultado acessando a propriedade do objeto."
 */
 
-let pessoa = {
-    nome: "João",
-    idade: 30,
-    profissao: "Desenvolvedor"
-};
-
-console.log(pessoa.nome);
+class heroi{
+    constructor(nome, classe){
+        this.nome = nome;
+        this.classe = classe;
+    }
+}
+function criarHeroi(){
+    // Capturando o que o usuário digitou/selecionou na tela
+    const nomeHeroi = document.getElementById('nomeInput').value;
+    const classeHeroi = document.getElementById('classeSelect').value;
+    //Criando o objeto dinamicamente com os valores dos campos
+    const novoHeroi = new heroi(nomeHeroi, classeHeroi);
+}
